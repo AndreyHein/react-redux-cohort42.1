580900c50470b4e2f98d842e0fc934d4210dbcb1
AndreyHein <soly.a@web.de>
2024-10-20 13:13:57 +0200

    homework16

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 // HOMEWORKS
 import Homework_15 from "homeworks/Homework_15/Homework_15"
+import Homework_16 from "homeworks/Homework_16/Homework_16"
 // LESSONS
 import Lesson_16 from "lessons/Lesson_16/Lesson_16"
 // CONSULTATION
 
 function App() {
   return (
-  // <Homework_15 />
-  <Lesson_16 />
+    // <Homework_15 />
+    <Homework_16 />
+    // <Lesson_16 />
   )
 }
 
